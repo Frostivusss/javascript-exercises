3759d4530bfa62f872233a88c6af831321816ea6
Frostivusss <jeromenas09@gmail.com>
2025-04-01 21:31:28 +0800

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
 
+
+const repeatString = function(stringWord, numberOfTimes) {
+
+    if(numberOfTimes < 0){
+
+        return finalRepeatedString = "ERROR";
+
+    }  else {
+
+        let finalRepeatedString = '';
+
+        for(let i = 0; i < numberOfTimes; i++){
+            finalRepeatedString += stringWord;
+        }
+
+    return finalRepeatedString;
+    }
 };
 
 // Do not edit below this line
