62c7ea22054f74eaa3e2483d2eea71d4f9037afd
Frostivusss <jeromenas09@gmail.com>
2025-04-02 01:07:27 +0800

    Complete 04_removeFromArray
    this was a fun exercise
    I felt like I can make it in this industry
    It feels so good when the dots connect

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(myArray, valueToDelete) {
+    const args = Array.from(arguments);
+    for (let j = 1; j < args.length; j++) {
+        for (let i = 0; i < args[0].length; i++) {
+            if (args[0][i] === args[j]) {
+                myArray.splice(i,1);
+                i--;
+            }
+        }
+    }
+
+    return myArray;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
