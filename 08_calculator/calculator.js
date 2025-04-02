c2a52e7277fd6639e3fca1639c7a178d96bd5492
Frostivusss <jeromenas09@gmail.com>
2025-04-02 16:35:56 +0800

    Completer 08_calculator
    Made an index.html for debugging since
    I still dont know how to use the debugger
    in the vscode

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1+num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1-num2;
 	
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let sum = 0;
+  for ( let i = 0; i < num.length; i++) {
+    sum += num[i];
+  }
+  return +sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let product = num[0];
+  for (let i = 1; i < num.length; i++) {
+      product *= num[i] ;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let power = num1;
+  for ( let i = 1; i < num2; i++) {
+    power *= num1;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    for (let i = num; i > 1; i--) {
+      factorial*= i-1;
+    }
+  }
+	return factorial;
 };
 
 // Do not edit below this line
