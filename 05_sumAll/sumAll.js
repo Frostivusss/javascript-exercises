616a5e3834a107cd690576a1c8013a887108847a
Frostivusss <jeromenas09@gmail.com>
2025-04-02 14:51:08 +0800

    Complete 05_sumAll
    learned how to check if the arguments passed to
    a fuction is Nan(Not a Number)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    let sum = 0;
+    
+    if (startInt < 0 || endInt < 0 || !(startInt % 1 === 0) ||
+     !(endInt % 1 === 0) || (typeof startInt !== "number") ||
+      (typeof endInt !== "number")) {
+        return "ERROR";
+    } else if (startInt < endInt) {
+        for (let i = startInt; i <= endInt; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    } else if (startInt > endInt) {
+        for (let i = endInt; i <= startInt; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    } else {
+
+    }
 
 };
 
